85d0d4b17a27388aa804c56f003f182842c23c16
Amit Deshwal <a3deswal@gmail.com>
2018-04-25 02:19:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onclick= function() {
   
  //Render it into a corrrect span tag
  counter = counter + 1;
- var span = getElementById("count");
+ var span = getElementById('count');
  span.innerHTML = counter.toString();
     
 };
