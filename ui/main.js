12ad2e607856a7eab546114a1a0b27a972dd790d
Amit Deshwal <a3deswal@gmail.com>
2018-04-25 14:39:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,10 +31,20 @@ button.onclick= function() {
 
 //Submit name
 var nameInput = document.getElementById('name');
-var name =nameInput.value;
+var name = nameInput.value;
 var submit = document.getELementById('submit_btn');
 submit.onclick = function(){
     //Reqst to server and send name
     
     //CApture a list of name
-}
+  var names = ['name1','name2','name3'];
+  var list='';
+  for (var i=0;i<names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.inerHTML = list;
+};
+  
+      
+    
